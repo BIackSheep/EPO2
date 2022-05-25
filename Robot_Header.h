7f92ea7a944945502a11804424a26bb884465d06
BlackSheep <48733284+BIackSheep@users.noreply.github.com>
2022-05-25 16:04:55 +0200

    Working again for Windows Code::Blocks

--- Robot_Header.h
+++ Robot_Header.h
@@ -9,12 +9,12 @@ extern int instruction[2];
 
 /*function prototypes*/
 int Lee (int station1, int station2);
-void shortest_route(int input_len, int *input_list, int *stationinput);
+void shortest_route(int*new_comb,int *shifted_comb,int nr_stations);
 void maze_init (int list_len, int* block_list);
 void print_matrix (void);
 void current_crossing(int* stationinput);
 void routeconcat(int *num, int n);
-int*permute(int*i,int h);
+int *permute(int*i,int h);
 void recursive_permute(int*i,int *j,int n);
 
 void initSio(HANDLE hSerial);
