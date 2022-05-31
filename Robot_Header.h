4103c787a3c4aa01bcfefb06e6af9d748f53c737
BlackSheep <S.P.N.Schaap@student.tudelft.nl>
2022-05-31 13:31:10 +0200

    Integratie Edin/Nicky code

--- Robot_Header.h
+++ Robot_Header.h
@@ -2,24 +2,24 @@
 #define ROBOT_HEADER_H_INCLUDED
 
 /*for HANDLE type*/
-#include <Windows.h>
+//#include <Windows.h>                              xcode
 
 extern int detection[2];
 extern int instruction[2];
 
 /*function prototypes*/
 int Lee (int station1, int station2);
-void shortest_route(int input_len, int *input_list, int *stationinput);
+void shortest_route(int*new_comb,int *shifted_comb,int nr_stations);
 void maze_init (int list_len, int* block_list);
 void print_matrix (void);
 void current_crossing(int* stationinput);
 void routeconcat(int *num, int n);
-int*permute(int*i,int h);
+int *permute(int*i,int h);
 void recursive_permute(int*i,int *j,int n);
 
-void initSio(HANDLE hSerial);
-int readByte(HANDLE hSerial, char *buffRead);
-int writeByte(HANDLE hSerial, char *buffWrite);
-int zigbee(void);
+//void initSio(HANDLE hSerial);                         xcode x4
+//int readByte(HANDLE hSerial, char *buffRead);
+//int writeByte(HANDLE hSerial, char *buffWrite);
+//int zigbee(void);
 
 #endif // ROBOT_HEADER_H_INCLUDED
